229ddc10eae926f24500eca27d4c33f78a6b9846
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-05-02 21:47:56 -0500

    Practicing functions by Creating a Color Generation Function for Markers

--- map1.py
+++ map1.py
@@ -7,7 +7,14 @@ lat =list(data["LAT"])  # Extrae la columna "latitud" del DataFrame y la convier
 lon =list(data["LON"])  # Extrae la columna "longitud" del DataFrame y la convierte en una lista
 name = list(data["NAME"])  
 genre = list(data["TYPE"]) 
+status = list(data["STATUS"])
 
+def color_producer(status):
+    if status == "Abierto":
+        return 'blue'
+    elif status == "Cerrado":
+        return 'red'
+    
 # Crea un mapa base centrado en Bogotá, Colombia, con un nivel de zoom inicial de 6
 map = folium.Map(
     location=[4.6031, -74.072],  # Coordenadas del centro del mapa (latitud, longitud)
@@ -19,15 +26,15 @@ map = folium.Map(
 fg = folium.FeatureGroup(name="My Map")
 
 # Agrega un marcador al grupo de características
-for lt, ln, nm, gn in zip(lat, lon, name, genre):
+for lt, ln, nm, gn, sts in zip(lat, lon, name, genre, status):
     fg.add_child(folium.Marker(
         location=[lt, ln],  # Coordenadas del marcador (latitud, longitud)
         popup=nm +" "+ gn ,    # Mensaje emergente que aparece al hacer clic en el marcador
-        icon=folium.Icon(color="green")  # Ícono del marcador con color verde
+        icon=folium.Icon(color=color_producer(sts))  # Ícono del marcador con color verde
     ))
 
 # Agrega el grupo de características al mapa principal
 map.add_child(fg)
 
-# Guarda el mapa generado en un archivo HTML llamado "map1.html"
-map.save("map1.html")
+# Guarda el mapa generado en un archivo HTML llamado "Map1.html"
+map.save("Map1.html")
