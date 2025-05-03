e287f31e4e50cd9b0bdfc48e5cf5249de2e8d0a4
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-05-02 20:47:16 -0500

    Practicing File Processing by Adding Markers from Files

--- map1.py
+++ map1.py
@@ -1,5 +1,10 @@
 
 import folium  # Importa la biblioteca folium para crear mapas interactivos
+import pandas
+
+data = pandas.read_csv("bares_rock_bogota.txt")  # Lee un archivo CSV llamado "data.csv" y lo almacena en un DataFrame
+lat =list(data["LAT"])  # Extrae la columna "latitud" del DataFrame y la convierte en una lista
+lon =list(data["LON"])
 
 # Crea un mapa base centrado en Bogotá, Colombia, con un nivel de zoom inicial de 6
 map = folium.Map(
@@ -12,9 +17,9 @@ map = folium.Map(
 fg = folium.FeatureGroup(name="My Map")
 
 # Agrega un marcador al grupo de características
-for coordinates in [[4.6031, -74.072],[4.6500, -74.0637]]:
+for lt, ln in zip(lat, lon):
     fg.add_child(folium.Marker(
-        location=coordinates,  # Coordenadas del marcador (latitud, longitud)
+        location=[lt, ln],  # Coordenadas del marcador (latitud, longitud)
         popup="Hi I am a Marker",    # Mensaje emergente que aparece al hacer clic en el marcador
         icon=folium.Icon(color="green")  # Ícono del marcador con color verde
     ))
